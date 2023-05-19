6809f6da063d65905ccfdad6d986f8b65c2e1303
NataljaBadulina <sannata@inbox.ru>
2023-05-19 18:18:25 +0300

    signals about new products adding/changing/

--- jdproject/jd_app/apps.py
+++ jdproject/jd_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class JdappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'jd_app'
+
+    def ready(self):
+        from . import signals     # выполнение модуля -> регистрация сигналов
